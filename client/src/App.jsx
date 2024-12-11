8b3b283cfbf70cf44a1b7761d42776555dac52c2
sushantraj18 <codexsushant@gmail.com>
2024-12-11 14:43:56 +0530

    little transition added in pages using gsap

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,17 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import Login from './pages/Login'
-import Signup from './pages/Signup'
-import BuyerDashboard from './pages/BuyerDashboard'
-import SellerDashboard from './pages/SellerDashboard'
+// import GsapTransition from './components/GsapTransition'
+
 import Navbar from './components/Navbar'
+import GsapTransition from './components/GsapTransition'
 
 function App() {
   return (
     <>
     <BrowserRouter>
     <Navbar/>
-    <Routes>
-      <Route path='/' element={<Home/>} />
-      <Route path='/login' element={<Login/>} />
-      <Route path='/signup' element={<Signup/>} />
-      <Route path='/buyer/profile' element={<BuyerDashboard/>} />
-      <Route path='/buyer/profile' element={<BuyerDashboard/>} />
-      <Route path='/seller/profile' element={<SellerDashboard/>} />
-    </Routes>
+
+    <GsapTransition/>
     </BrowserRouter>
     </>
   )
---
+++ client/src/components/GsapTransition.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useRef } from 'react'
+import Home from '../pages/Home'
+import Login from '../pages/Login'
+import Signup from '../pages/Signup'
+import BuyerDashboard from '../pages/BuyerDashboard'
+import SellerDashboard from '../pages/SellerDashboard'
+import {Routes,Route, useLocation} from 'react-router-dom'
+import gsap from 'gsap'
+
+
+function GsapTransition() {
+    const nodeRef = useRef(null)
+    const location = useLocation();
+    // console.log("location is ",location)
+
+    useEffect(()=>{
+        if(nodeRef.current){
+            gsap.fromTo(nodeRef.current,{opacity:0},{opacity:1,duration:1})
+        }
+    },[location])
+
+  return (
+    <div ref={nodeRef}>
+    <Routes location={location}>
+      <Route path='/' element={<Home/>} />
+      <Route path='/login' element={<Login/>} />
+      <Route path='/signup' element={<Signup/>} />
+      <Route path='/buyer/profile' element={<BuyerDashboard/>} />
+      <Route path='/buyer/profile' element={<BuyerDashboard/>} />
+      <Route path='/seller/profile' element={<SellerDashboard/>} />
+    </Routes>
+    </div>
+  )
+}
+
+export default GsapTransition
